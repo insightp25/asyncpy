a02af37a8554d7e16309cc3b94430448a94e5773
insightp25 <71416000+insightp25@users.noreply.github.com>
2022-02-24 11:24:00 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from app.book_scraper import NaverBookScraper
 BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI()
-templates = Jinja2Templates(directory=BASE_DIR / "templates")
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 
 @app.get("/", response_class=HTMLResponse)
